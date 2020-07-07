ec6d3c81cbe657f3aba63fd09a4f56176a8a82ba
waynewbishop <wayne.bishop@arbutusinc.com>
2020-07-06 19:10:07 -0700

    Starting work on the interactive SwiftUI interface..

--- Playgrounds/Sample.playground/Contents.swift
+++ Playgrounds/Sample.playground/Contents.swift
@@ -3,8 +3,44 @@
  # Challenges
  */
 
+import PlaygroundSupport
 import Structures
+import SwiftUI
 
-//..create a new table
-let table = Table(8)
+
+struct StackView: View {
+    
+    var element: String = ""
+
+    
+    //default initialization
+    init(_ item: String = "") {
+        element = item
+    }
+    
+    var body: some View {
+        
+        List(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+            Spacer()
+            ZStack {
+                    RoundedRectangle(cornerRadius: 10)
+                       // .stroke(lineWidth: 12)
+                        .foregroundColor(.orange)
+                        .frame(width: 195.0, height: 100.0)
+                    
+                    Text(element)
+                        .foregroundColor(.white)
+                        .font(.system(.largeTitle, design: .rounded))
+                        .fontWeight(.bold)
+                    
+            }
+            Spacer()
+        }
+    }
+}
+
+
+
+PlaygroundPage.current.setLiveView(StackView("8"))
+
 
